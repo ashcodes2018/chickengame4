d913018e118608f53a3f1df9b32ceb6bfe149a37
Aswathy <ashcodes2018@gmail.com>
2018-02-13 17:46:36 -0500

    working game

--- game4.js
+++ game4.js
@@ -8,43 +8,45 @@
     var playerWinsBar=document.getElementById("userwin");
     var grantHealthBar=document.getElementById("granthealth");
     var nameOfThePlayer =document.getElementById("username");
-    var message = document.getElementById("gamestatusmsg");
+    var message = document.getElementById("consolemsg");
     var hiddenPage=document.getElementsByClassName("hiddencontentofthepage");
     var theButtons=document.getElementById("gamebuttons");
-
+    //console.log(userObject.heal());
   var userObject={
 
-  userName:null,
-  userHealth:40,
-  userHealsRemaining:2,
-  userWins:0,
-  heal:function()
-  {
-    var rand= Math.floor((Math.random()*10)+1);
-    this.userHealth+=rand;
-    this.userHealsRemaining--;
-  },
+    userName:null,
+    userHealth:40,
+    userHealsRemaining:2,
+    userWins:0,
+    heal:function()
+      {
+          var rand= Math.floor((Math.random()*10)+1);
+          this.userHealth+=rand;
+          this.userHealsRemaining--;
+      },
    generateAttackDamageForGrant:function()
-   {
-     return Math.floor((Math.random()*3)+1);
+      {
+          return Math.floor((Math.random()*3)+1);
 
-  }
+        }
 };
 
-    var grantObject= {
-      grantName:"Grant the Mighty Chicken",
-      grantHealth:10,
-      generateAttackDamageForUser:function()
-      {
-          return Math.floor((Math.random()*5)+1);
-      }
+    var grantObject=
+    {
+          grantName:"Grant the Mighty Chicken",
+          grantHealth:10,
+          generateAttackDamageForUser:function()
+          {
+            return Math.floor((Math.random()*5)+1);
+          }
   };
 
   start.onclick=function()
     {
         userObject.userName= prompt("Enter your name");
-        nameOfThePlayer.textContent = userObject.userName
-        hiddenpage.classList.remove("hiddencontentofthepage");
+        nameOfThePlayer.textContent = userObject.userName;
+        hiddenPage[0].classList.remove("hiddencontentofthepage");
+
     };
 
   attackButton.onclick=function()
@@ -52,8 +54,8 @@
 
     if(userObject.userHealth>0 && userObject.userWins<5)
       {
-        userObject.userHealth -= userObject.generateAttackDamageForUser();
-        grantObject.grantHealth-=grantObject.generateAttackDamageForGrant();
+        userObject.userHealth -= grantObject.generateAttackDamageForUser();
+        grantObject.grantHealth-=userObject.generateAttackDamageForGrant();
         updateDisplay();
         updateMessage(nameOfThePlayer.innerHTML+" has "+userObject.userHealth+" health left. "+grantObject.grantName+" has "+grantObject.grantHealth+" health left.");
       };
@@ -75,19 +77,24 @@
             theButtons.style.display = "none";
         }
 };
-
+function updateDisplay()
+  {
+      playerHealthBar.value = userObject.userHealth;
+      grantHealthBar.value = grantObject.grantHealth;
+      playerHealBar.value = userObject.userHealsRemaining;
+      playerWinsBar.value = userObject.userWins;
+  };
     healButton.onclick = function()
     {
         if(userObject.userHealsRemaining > 0)
         {
-            userObject.userHealth += userObject.heal();
+            userObject.heal();
             updateMessage(nameOfThePlayer.innerHTML + " has " + userObject.userHealsRemaining + " heals remaining.");
             updateDisplay();
         }
         else if(userObject.userHealsRemaining === 0)
         {
             updateMessage("Sorry!! You have used all your lives");
-            //theButtons.style.display = "none";
         }
     };
     quitButton.onclick = function()
@@ -95,15 +102,10 @@
         updateMessage("Game over");
         theButtons.style.display = "none";
     };
-  function updateDisplay()
-    {
-        playerHealthBar.value = userObject.userHealth;
-        grantHealthBar.value = grantObject.grantHealth;
-        playerHealBar.value = userObject.userHealsRemaining;
-        playerWinsBar.value = userObject.userWins;
-    };
 
-  function updateMessage(newMessage) {
-        message.innerText = newMessage;
-    };
+    function updateMessage(newMessage) {
+          message.innerText = newMessage;
+
+      };
+
  })();
